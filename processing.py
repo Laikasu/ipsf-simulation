fixed phase sign

processing.py
@@ -1,11 +1,18 @@
 import numpy as np
 
-def background_subtracted(data, background):
-    diff = np.divide(np.subtract(data, background, dtype=np.int32), background, dtype=np.float64)
-    return diff
+def background_subtracted(data, background, background_norm=True):
+    if background_norm:
+        return np.divide(np.subtract(data, background, dtype=np.int32), background, dtype=np.float64)
+    else:
+        return np.subtract(data, background, dtype=np.int32)
 
-def float_to_mono(data):
-    data = np.clip(data, -1, 1)
+def float_to_mono(data, normalize=False):
+    if normalize:
+        d = (data - np.min(data))
+        data = (d/np.max(d))*2-1
+    else:
+        data = np.clip(data, -1, 1)
+
     return ((data+1)*32767).astype(np.uint16)
 
 
@@ -14,8 +21,8 @@ def common_background(backgrounds):
     for i in range(len(backgrounds)):
         for j in range(len(backgrounds)):
             if (i < j):
-                diff = np.divide(np.subtract(backgrounds[i], backgrounds[j], dtype=np.int32), backgrounds[j], dtype=np.float64)
-                weight = np.exp(-np.abs(diff)/0.1)
+                diff = np.divide(np.subtract(backgrounds[i], backgrounds[j], dtype=np.int32), backgrounds[j],dtype=np.float64)
+                weight = np.exp(-np.abs(diff)/0.01)
                 output_weights[i] = np.maximum(weight, output_weights[i])
                 output_weights[j] = np.maximum(weight, output_weights[j])
     

windows/mainwindow.py
@@ -178,10 +178,11 @@ class MainWindow(QMainWindow):
         # self.update_statistics_timer.start()
     
     def update_psf(self, params: DesignParams):
+        s = self.parameter_window
         camera = Camera(params)
         pxsize_obj = params.pxsize/params.magnification
-        scatter_field = calculate_scatter_field(params)
-        self.intensity = calculate_intensities(scatter_field, params, camera, r_resolution=self.parameter_window.rresolution)
+        scatter_field = calculate_scatter_field(params, multipolar=s.multipolar, angular=s.angular)
+        self.intensity = calculate_intensities(scatter_field, params, camera, r_resolution=s.rresolution)
         self.display.update_image(self.intensity, pxsize_obj)
     
     def sweep(self, params: DesignParams, param_name: str, param: np.ndarray):