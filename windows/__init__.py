fe0b4a6875c1bc2b533b3c660ead91a513cddc3d
casperdely <casper.dely@hotmail.com>
2025-11-14 14:34:47 +0100

    updates

---
+++ windows/__init__.py
@@ -0,0 +1,10 @@
+#__init__.py
+from .mpl_canvas import MplCanvas
+from .mpl_plot import PlotWindow
+from .parameter_window import ParameterWindow
+
+__all__ = [
+    "MplCanvas",
+    "PlotWindow",
+    "ParameterWindow",
+]
